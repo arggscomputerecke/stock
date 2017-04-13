b6548fc6e24db97ddf4be6d0ba8f9ac03257fbc3
ArggsComputerEcke <arggscomputerecke@gmail.com>
2017-04-13 22:11:04 +0200

    factory first version

--- bin_readin/main.cxx
+++ bin_readin/main.cxx
@@ -6,6 +6,7 @@
 #include <math.h>
 #include "TDatime.h"
 #include <memory>
+#include <algorithm>
 
 
 using namespace std;
@@ -45,20 +46,41 @@ std::string remove_path_and_extension(const std::string& buffer) {
   auto t = buffer.substr(lastslash + 1, i - lastslash - 1);
   return t;
 }
+std::string remove_white_spaces(const std::string& name) {
+  std::string ret(name);
+  std::replace(ret.begin(), ret.end(), ' ', '_'); 
+  return ret;
+}
 
 class readinBase {
 public:
-  readinBase(TTree* tree, char delim) :m_tree(tree), m_delim(delim) {}
+  readinBase(TTree* tree, const std::string& name, char delim) :m_tree(tree), m_name(name), m_delim(delim) {}
   virtual bool readBuffer(std::istream&) = 0;
   TTree *m_tree;
   std::string m_buffer;
+  std::string m_name;
   const char m_delim;
 };
 
+typedef readinBase*(*reader_creator)(TTree * tree, const std::string& name, char delim);
+using r_map = std::map<std::string, reader_creator>;
+
+r_map& get_register() {
+  static r_map map;
+  return map;
+}
+
+void register_readin(const std::string& type, reader_creator fun) {
+  get_register()[type] = fun;
+}
+std::shared_ptr<readinBase> create_readin(const std::string& type, TTree * tree, const std::string& name, char delim = ',') {
+  return std::shared_ptr<readinBase>(  get_register()[type](tree, name, delim));
+}
+
 class readin_doubles :public readinBase {
 public:
-  readin_doubles(TTree * tree, const char* name, char delim = ',') :readinBase(tree, delim) {
-    tree->Branch(name, &m_data);
+  readin_doubles(TTree * tree, const std::string& name, char delim = ',') :readinBase(tree,name, delim) {
+    tree->Branch(remove_white_spaces(name).c_str(), &m_data);
   }
   virtual bool readBuffer(std::istream& in) {
     getline(in, m_buffer, m_delim);
@@ -69,13 +91,25 @@ public:
     m_data = str2double(m_buffer);
   };
   double m_data;
-
+  
 };
+namespace {
+  readinBase* create_readin_doubles(TTree * tree, const std::string& name, char delim) {
+    return new readin_doubles(tree, name, delim);
+  }
+  class __register_readin_doubles {
+  public:
+    __register_readin_doubles() {
+      register_readin("D", &create_readin_doubles);
+    }
+  } ___regD;
+}
+
 
 class readin_dates :public readinBase {
 public:
-  readin_dates(TTree * tree, const char* name, char delim = ',') :readinBase(tree, delim) {
-    tree->Branch(name, &m_data);
+  readin_dates(TTree * tree, const std::string& name, char delim = ',') :readinBase(tree,name, delim) {
+    tree->Branch(remove_white_spaces(name).c_str(), &m_data);
   }
   virtual bool readBuffer(std::istream& in) {
     getline(in, m_buffer, m_delim);
@@ -90,6 +124,57 @@ public:
 
 };
 
+namespace {
+  readinBase* create_readin_dates(TTree * tree, const std::string& name, char delim) {
+    return new readin_dates(tree, name, delim);
+  }
+  class __register_readin_dates {
+  public:
+    __register_readin_dates() {
+      register_readin("T", &create_readin_dates);
+    }
+  } ___regT;
+}
+
+
+
+
+std::vector<std::string> split(const std::string& input, char delimiter = ',') {
+  vector<string> result;
+  std::stringstream ss;
+  ss.str(input);
+  std::string item;
+  while (std::getline(ss, item, delimiter)) {
+    if (item.empty())
+    {
+      continue;
+    }
+   result.push_back(item);
+  }
+  return result;
+
+}
+
+std::vector<std::shared_ptr<readinBase>> create_processors(const std::string& header, const std::string& formating,TTree *tree) {
+  
+  
+  auto s_formating = split(formating,'%');
+  s_formating.back() += '\n';
+  
+  auto s_header = split(header, s_formating[0].back());
+
+  if (s_formating.size()!=s_header.size()){
+    throw std::exception("size of header and size of formating string not identical");
+  }
+ 
+  std::vector<std::shared_ptr<readinBase>> processors;
+  for (int i =0 ;i<s_formating.size();++i)
+  {
+    processors.push_back(create_readin(s_formating[i].substr(0,1), tree, s_header.at(i), s_formating[i].back()));
+  }
+  return processors;
+}
+
 int main(int argc, char **argv) {
   CmdLine cmd("ProcessFile", ' ', "0.1");
   ValueArg<std::string> FileNameArg("i", "inFile", "csv file", true, "", "string");
@@ -98,40 +183,36 @@ int main(int argc, char **argv) {
 
   ValueArg<std::string> outFile("o", "outFile", "output ROOT file", true, "", "string");
   cmd.add(outFile);
+
+  ValueArg<std::string> str_format("f", "formating", "gives an indication on how to interpret the individual columns", true, "", "string");
+  cmd.add(str_format);
   cmd.parse(argc, argv);  //terminates on error
-
-
-
 
   TFile outF(outFile.getValue().c_str(), "Recreate");
 
 
   TTree tree(remove_path_and_extension(FileNameArg.getValue()).c_str(), "data");
 
-  std::vector<std::shared_ptr<readinBase>> processors;
-  processors.push_back( std::make_shared<readin_dates>( &tree,"Date" ));
-  processors.push_back(std::make_shared<readin_doubles>(&tree, "Open"));
-  processors.push_back(std::make_shared<readin_doubles>(&tree, "High"));
-  processors.push_back(std::make_shared<readin_doubles>(&tree, "Low"));
-  processors.push_back(std::make_shared<readin_doubles>(&tree, "Close"));
-  processors.push_back(std::make_shared<readin_doubles>(&tree, "Volume"));
-  processors.push_back(std::make_shared<readin_doubles>(&tree, "AdjClose",'\n'));
+
   
 
 
   std::ifstream in(FileNameArg.getValue().c_str());
 
   std::string buffer;
-
   getline(in, buffer);
+  auto s = split(buffer);
 
+  auto processors = create_processors(buffer, str_format.getValue(),&tree);
+
+  int i = 0;
   while (in.good())
   {
     for (auto&e:processors)
     {
       e->readBuffer(in);
     }
-
+    i++;
     tree.Fill();
 
   }
